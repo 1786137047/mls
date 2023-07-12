80b4dd851ff36e69ccfd97382e74a4f87a85f198
1786137047 <33090354+1786137047@users.noreply.github.com>
2023-07-12 11:27:03 +0800

    Add files via upload

--- dfrc.sh
+++ dfrc.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
-#东风日产
+# 东风日产
 #新用户请走邀请链接下载可得30积分https://oneapph5.dongfeng-nissan.com.cn/oneapp/inviteBindCar?urlData=2HuCYYTn5BB%2FrBQI%2FpKH%2FZX0VYSPsqBWb6opaO9uaizK9x%2BTGaECIC%2FHeRf19vcwyYNVb%2B%2BLgUaXXPPze5JfAg%3D%3D
-#抓app，域名oneapph5.dongfeng-nissan.com.cn抓token，sign，noncestr，timestamp的值
-#青龙创建环境变量，变量名dfrcck，格式为token@sign@noncestr@timestamp
-#by-莫老师，版本3.0
+#抓app，域名oneapph5.dongfeng-nissan.com.cn抓token的值
+#青龙创建环境变量，变量名dfrcck值为token
+#by-莫老师，v3.0
 #cron:0 0 * * *
-zh=($(echo $dfrcck | sed 's/&/ /g'))
+################################
+
+ck=($(echo $dfrcck | sed 's/&/ /g'))
 url=oneapph5.dongfeng-nissan.com.cn
 if [ ! -f "testtzid" ]; then
 echo $[$RANDOM%200000+1000000] >testtzid
 fi
 getid(){
 testtzid=$(cat testtzid)
-msg=$(echo -e $(curl -s -X GET -H "token: $ck" -H "Content-Type: application/json" -H "Host: $url" "https://$url/mb-gw/dndc-gateway/community/api/v2/feeds/$testtzid" -k) | sed 's/,/\n/g' | grep "msg" | awk -F ":" '{print $2}' | sed 's/\"//g')
+msg=$(echo -e $(curl -s -X GET -H "token: ${ck[$s]}" -H "Content-Type: application/json" -H "Host: $url" "https://$url/mb-gw/dndc-gateway/community/api/v2/feeds/$testtzid" -k) | sed 's/,/\n/g' | grep "msg" | awk -F ":" '{print $2}' | sed 's/\"//g')
 if echo "$msg" | grep -q "成功"; then
 tzid=$testtzid
 let testtzid++
@@ -26,7 +28,7 @@ fi
 pl(){
 comment=$(curl -s "https://v1.hitokoto.cn/?encode=text" -k)
 length=$(($(echo "$tzid$comment" | awk '{print length($0)}')+58))
-msg=$(echo -e "$(curl -s -X POST -H "token: $ck" -H "Content-Type: application/json" -H "Content-Length: $length" -H "Host: $url" -d '{"commentable_type":"feeds","commentable_id":"'$tzid'","body":"'$comment'"}' "https://$url/mb-gw/dndc-gateway/community/api/v2/comments" -k | sed 's/,/\n/g' | grep "msg" | awk -F ":" '{print $2}' | sed 's/\"//g')")
+msg=$(echo -e "$(curl -s -X POST -H "token: ${ck[$s]}" -H "Content-Type: application/json" -H "Content-Length: $length" -H "Host: $url" -d '{"commentable_type":"feeds","commentable_id":"'$tzid'","body":"'$comment'"}' "https://$url/mb-gw/dndc-gateway/community/api/v2/comments" -k | sed 's/,/\n/g' | grep "msg" | awk -F ":" '{print $2}' | sed 's/\"//g')")
 if echo "$msg" | grep -q "成功"; then
 echo "日产账号$s第$i次$msg"
 elif [ "$msg" = token无效，请重新登录 ];then
@@ -42,7 +44,7 @@ curl -s -o dfrc.json http://43.156.44.48/api/yy.php
 title=$(cat dfrc.json | awk -F "," '{print $1}')
 content=$(cat dfrc.json | awk -F "," '{print $2}')
 length=$(($(echo $content$title | awk '{print length($0)}')+159))
-msg=$(echo -e "$(curl -s -X POST -H "clientid: nissanapp" -H "token: $ck" -H "Content-Type: application/json" -H "Content-Length: $length" -H "Host: $url" -d '{"feed_mark":'$(date +%s)'000,"feed_title":"'$title'","themes":[],"feeds_type":2,"feed_from":2,"app_feed_content":[{"content":{"height":0,"text":"'$content'","width":0},"type":1}]}' "https://$url/mb-gw/dndc-gateway/community/api/v2/feeds" -k | sed 's/,/\n/g' | grep "msg" | awk -F ":" '{print $2}' | sed 's/\"//g')")
+msg=$(echo -e "$(curl -s -X POST -H "clientid: nissanapp" -H "token: ${ck[$s]}" -H "Content-Type: application/json" -H "Content-Length: $length" -H "Host: $url" -d '{"feed_mark":'$(date +%s)'000,"feed_title":"'$title'","themes":[],"feeds_type":2,"feed_from":2,"app_feed_content":[{"content":{"height":0,"text":"'$content'","width":0},"type":1}]}' "https://$url/mb-gw/dndc-gateway/community/api/v2/feeds" -k | sed 's/,/\n/g' | grep "msg" | awk -F ":" '{print $2}' | sed 's/\"//g')")
 if echo "$msg" | grep -q "成功"; then
 echo "日产账号$s第$i次$msg"
 elif [ "$msg" = token无效，请重新登录 ];then
@@ -60,27 +62,20 @@ for i in $(seq 4)
 do
 for s in $(seq 0 1 $((${#ck[@]}-1)))
 do
-ck=$(echo ${zh[$s]} | awk -F "@" '{print $1}')
 ft
 done
 wait
 done
 for s in $(seq 0 1 $((${#ck[@]}-1)))
 do
-ck=$(echo ${zh[$s]} | awk -F "@" '{print $1}')
-sign=$(echo ${zh[$s]} | awk -F "@" '{print $2}')
-noncestr=$(echo ${zh[$s]} | awk -F "@" '{print $3}')
-time=$(echo ${zh[$s]} | awk -F "@" '{print $4}')
-req=$(head /dev/urandom | tr -dc a-z0-9 | head -c 32)
-msg=$(echo -e "$(curl -s -X POST -H "clientid: nissanapp" -H "sign: $sign" -H "range: 1" -H "noncestr: $noncestr" -H "token: $ck" -H "appSkin: NISSANAPP" -H "timestamp: $time" -H "Content-Types application/json" -H "Content-Length: 67" -H "Host: $url" -d '{"version":"202304","requestId":"'$req'"}' "https://$url/mb-gw/vmsp-me/ly/busicen/member/reward/pointsreturn/memberPointsRechargetRequestSign" -k | sed 's/,/\n/g' | grep "msg" | awk -F ":" '{print $2}' | sed 's/\"//g')")
+msg=$(echo -e "$(curl -s -X GET -H "token: ${ck[$s]}" -H "Host: $url" "https://$url/mb-gw/dndc-gateway/community/api/v2/feed-themes/get_index" -k | sed 's/,/\n/g' | grep "msg" | awk -F ":" '{print $2}' | sed 's/\"//g')")
 if echo "$msg" | grep -q "成功"; then
 echo "日产账号$s$msg"
-curl -s -X POST -H "clientid: nissanapp" -H "sign: $sign" -H "range: 1" -H "noncestr: $noncestr" -H "token: $ck" -H "appSkin: NISSANAPP" -H "timestamp: $time" -H "Content-Types application/json" -H "Content-Length: 67" -H "Host: $url" -d '{"version":"202304","requestId":"'$req'"}' "https://$url/mb-gw/vmsp-me/ly/busicen/member/reward/pointsreturn/getSignResul" -k
 for i in $(seq 5)
 do
 getid
 pl
-echo -e "日产账号$s第$i次$(curl -s -X POST -H "appVersion: 3.0.0" -H "token: $ck" -H "Content-Length: 2" -H "Host: $url" -d "{}" "https://$url/mb-gw/dndc-gateway/community/api/v2/feeds/$tzid/like" -k | sed 's/,/\n/g' | grep "msg" | awk -F ":" '{print $2}' | sed 's/\"//g')"
+echo -e "日产账号$s第$i次$(curl -s -X POST -H "appVersion: 3.0.0" -H "token: ${ck[$s]}" -H "Content-Length: 2" -H "Host: $url" -d "{}" "https://$url/mb-gw/dndc-gateway/community/api/v2/feeds/$tzid/like" -k | sed 's/,/\n/g' | grep "msg" | awk -F ":" '{print $2}' | sed 's/\"//g')"
 done
 elif [ "$msg" = token无效，请重新登录 ];then
 echo "日产账号$s的ck失效请重新抓"
